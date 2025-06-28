df8ba1c4fac5f558e3a4c550c13e4872a868fea0
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-06-28 13:14:30 +0900

    routingに分析を追加

--- lib/core/routing/observers/navigation_observer.dart
+++ lib/core/routing/observers/navigation_observer.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,23 +10,35 @@ class NavigationObserver extends NavigatorObserver {
   // ignore: unused_field
   final Ref _ref;
 
+  // Firebase Analytics instance for tracking navigation events
+  final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
+
+  // Firebase Crashlytics instance for breadcrumb tracking
+  final FirebaseCrashlytics _crashlytics = FirebaseCrashlytics.instance;
+
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPush(route, previousRoute);
     _logNavigation('push', route, previousRoute);
-    // TODO: Implement analytics tracking with _ref.read(analyticsProvider)
+    _trackScreenView(route);
   }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPop(route, previousRoute);
     _logNavigation('pop', route, previousRoute);
+    if (previousRoute != null) {
+      _trackScreenView(previousRoute);
+    }
   }
 
   @override
   void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
     super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
     _logNavigation('replace', newRoute, oldRoute);
+    if (newRoute != null) {
+      _trackScreenView(newRoute);
+    }
   }
 
   @override
@@ -44,15 +58,30 @@ class NavigationObserver extends NavigatorObserver {
     // Debug logging
     debugPrint('Navigation: $action from $previousRouteName to $routeName');
 
-    // TODO: Send to GA4 analytics
-    // final analytics = _ref.read(analyticsProvider);
-    // analytics.logScreenView(screenName: routeName);
+    // Send navigation events to Firebase Analytics
+    _analytics.logEvent(
+      name: 'navigation',
+      parameters: {
+        'action': action,
+        'from_screen': previousRouteName,
+        'to_screen': routeName,
+      },
+    );
 
-    // TODO: Send navigation events to Crashlytics breadcrumbs
-    // final crashlytics = _ref.read(crashlyticsReporterProvider);
-    // crashlytics.addBreadcrumb(
-    //   message: 'Navigation: $action to $routeName',
-    //   category: 'navigation',
-    // );
+    // Send navigation events to Crashlytics breadcrumbs
+    _crashlytics.log(
+      'Navigation: $action from $previousRouteName to $routeName',
+    );
+  }
+
+  /// Track screen view events for Firebase Analytics
+  void _trackScreenView(Route<dynamic> route) {
+    final routeName = route.settings.name;
+    if (routeName != null && routeName.isNotEmpty) {
+      _analytics.logScreenView(
+        screenName: routeName,
+        screenClass: route.runtimeType.toString(),
+      );
+    }
   }
 }
