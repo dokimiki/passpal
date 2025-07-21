5a2eea63ecd1f4b572a7dcfe5091dabdef817bc8
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-07-21 12:18:16 +0900

    新しいパッケージ導入

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "app.chukyopasspal.passpal"
-    compileSdk = 21
+    compileSdk = 34
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -33,12 +33,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "app.chukyopasspal.passpal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
